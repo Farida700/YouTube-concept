583c7e92c78eb2e42484a16b4bd064288673a211
Фарида <74248789+Farida700@users.noreply.github.com>
2021-04-11 23:36:17 +0600

    git commite -M

---
+++ js/main.js
@@ -0,0 +1,54 @@
+const swiper = new Swiper(".channel_slider", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1600: {
+      slidesPerView: 5,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    800: {
+      slidesPerView: 2,
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".channel-button-next",
+    prevEl: ".channel-button-prev",
+  },
+});
+const firstSwiper = new Swiper(".first_channel_slider", {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3,
+    },
+    1100: {
+      slidesPerView: 2,
+    },
+    navigation: {
+      nextEl: ".first-channel-button-next",
+      prevEl: ".first-channel-button-prev",
+    },
+  },
+});
+const secondSwiper = new Swiper(".second_channel_slider", {
+  loop: true,
+  slidesPerView: 6,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".second-channel-button-next",
+    prevEl: ".second-channel-button-prev",
+  },
+});
